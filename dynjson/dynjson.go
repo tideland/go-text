00cb8f62d1dda182d658695b97886edacdcbfca1
themue <frank.mueller@themue.dev>
2021-05-14 21:37:55 +0000

    Add Document with parsing
    Signed-off-by: themue <frank.mueller@themue.dev>

---
+++ dynjson/dynjson.go
@@ -0,0 +1,46 @@
+// Tideland Go Text - Dynamic JSON
+//
+// Copyright (C) 2021 Frank Mueller / Tideland / Oldenburg / Germany
+//
+// All rights reserved. Use of this source code is governed
+// by the new BSD license.
+
+package dynjson // import "tideland.dev/go/text/dynjson"
+
+//--------------------
+// IMPORTS
+//--------------------
+
+import (
+	"encoding/json"
+	"io"
+
+	"tideland.dev/go/trace/failure"
+)
+
+//--------------------
+// DOCUMENT
+//--------------------
+
+// Document represents one JSON document.
+type Document struct {
+	root interface{}
+}
+
+// Parse reads a raw document from a reader and returns it as
+// accessible document.
+func Parse(r io.Reader) (*Document, error) {
+	var bs []byte
+	if _, err := r.Read(bs); err != nil {
+		return nil, failure.Annotate(err, "connot read document to parse")
+	}
+	var root interface{}
+	if err := json.Unmarshal(bs, &root); err != nil {
+		return nil, failure.Annotate(err, "cannot unmarshal document")
+	}
+	return &Document{
+		root: root,
+	}, nil
+}
+
+// EOF
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	tideland.dev/go/audit v0.4.0
 	tideland.dev/go/dsa v0.3.1
 	tideland.dev/go/together v0.0.0-20200110213028-8f143e2737a6
-	tideland.dev/go/trace v0.0.0-20200110203012-77bd5be58e01
+	tideland.dev/go/trace v0.3.0
 )
